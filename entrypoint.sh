c3a4c093b474ca266f9533572c93b0837a16021b
Allen Shaw <lonsty@sina.com>
2020-06-15 14:12:11 +0800

    🐛 must set proxies to execute script on reboot

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Type `crontab -e` in the terminal, then add following line to it:
-# @reboot /path/to/entrypoint.sh
+# @reboot export https_proxy=https://user:pwd@host:port /path/to/entrypoint.sh
 
 /home/allen/.local/share/virtualenvs/qqmusic-cookie-setter-Vp3S7ezg/bin/python /mnt/data/workspace/git/packages/qqmusic-cookie-setter/scheduler.py
